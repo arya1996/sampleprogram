3c7344149a9f25596c42bc32cf22841b04ae723a
aryassasi <aryassasi@gmail.com>
2020-02-26 09:27:22 +0530

    first commit

---
+++ TestGit/src/com/git/app/Test.java
@@ -0,0 +1,16 @@
+package com.git.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+
+		//Program 1
+		int a=10,b=20,s;
+		s=a+b;
+		System.out.println("Sum is:" +s);
+		
+		
+	}
+
+}
